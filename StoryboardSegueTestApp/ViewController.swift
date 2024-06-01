585e282f4379b9492fa0be263df5d5df3a23a44d
wglee0511 <ranazang@naver.com>
2024-06-01 14:42:25 +0900

    Initial Commit

---
+++ StoryboardSegueTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryboardSegueTestApp
+//
+//  Created by racoon on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
