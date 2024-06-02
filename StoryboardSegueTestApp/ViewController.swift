728e8b4e05c3d6f8f30dd2ef58c2e9c5df10497b
wglee0511 <ranazang@naver.com>
2024-06-02 11:01:28 +0900

    feature: segue ui

--- StoryboardSegueTestApp/ViewController.swift
+++ StoryboardSegueTestApp/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func onPressMenualSegue(_ sender: Any) {
+        
+        performSegue(withIdentifier: SegueId.menualSegue.rawValue, sender: nil
+        )
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ constant.swift
@@ -0,0 +1,11 @@
+//
+//  constant.swift
+//  StoryboardSegueTestApp
+//
+//  Created by racoon on 6/1/24.
+//
+
+
+enum SegueId : String {
+    case menualSegue
+}
